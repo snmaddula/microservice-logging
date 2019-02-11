69605f97a6ef101bda54c38b40464f39d26b9762
snmaddula <snmaddula@outlook.com>
2019-02-11 12:17:41 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/ms/logging/MicroserviceLoggingApplication.java
@@ -0,0 +1,14 @@
+package snmaddula.ms.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceLoggingApplication.class, args);
+	}
+
+}
+
